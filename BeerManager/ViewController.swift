b47b63b3dea505f19b38a921ade24fb814492c33
Jorge Cruz <jorgecruz@usal.es>
2020-12-24 10:37:04 -0800

    Initial Commit

---
+++ BeerManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerManager
+//
+//  Created by  AlbaCruz on 24/12/2020.
+//  Copyright © 2020  AlbaCruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
