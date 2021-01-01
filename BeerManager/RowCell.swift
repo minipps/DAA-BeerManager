b3fbe37c4c1bfc01b06fa168ae832fc8a54ed342
Jorge Cruz <jorgecruz@usal.es>
2021-01-01 08:14:29 -0800

    added tableview parsing

---
+++ BeerManager/RowCell.swift
@@ -0,0 +1,15 @@
+//
+//  RowCell.swift
+//  BeerManager
+//
+//  Created by  AlbaCruz on 01/01/2021.
+//  Copyright © 2021  AlbaCruz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RowCell : UITableViewCell {
+    @IBOutlet weak var beerPhoto:UIImageView?
+    @IBOutlet weak var beerName:UILabel?
+}
--- BeerManager/ViewController.swift
+++ BeerManager/ViewController.swift
@@ -9,13 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var removeButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    var m = Model()
+    var editingStyle:UITableViewCell.EditingStyle?
     override func viewDidLoad() {
         super.viewDidLoad()
-        var m = Model()
         // Do any additional setup after loading the view.
+        // TODO: Hacer guardado en disco cada vez que se vaya a background.
+        self.editingStyle = UITableViewCell.EditingStyle.none;
+        self.tableView.dataSource = self
+        self.tableView.reloadData()
     }
 
+    @IBAction func addButtonAction(_ sender : Any) {
+        if (self.editingStyle == UITableViewCell.EditingStyle.none) {
+            self.editingStyle = UITableViewCell.EditingStyle.insert
+            self.removeButton.isEnabled = false
+            self.addButton.setTitle("DONE", for: .normal)
+            self.tableView.setEditing(true, animated: true)
+        } else {
+            self.editingStyle = UITableViewCell.EditingStyle.none
+            self.removeButton.isEnabled = true
+            self.addButton.setTitle("Add beer", for: .normal);
+            self.tableView.setEditing(false, animated: true);
+        }
+    }
+
+    @IBAction func removeButtonAction(_ sender : Any) {
+        if (self.editingStyle == UITableViewCell.EditingStyle.none) {
+            self.editingStyle = UITableViewCell.EditingStyle.delete
+            self.addButton.isEnabled = false
+            self.removeButton.setTitle("DONE", for: .normal)
+            self.tableView.setEditing(true, animated: true)
+        } else {
+            self.editingStyle = UITableViewCell.EditingStyle.none
+            self.addButton.isEnabled = true
+            self.removeButton.setTitle("Remove beer", for: .normal);
+            self.tableView.setEditing(false, animated: true);
+        }
+    }
+}
+
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return m.cervezas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let n = indexPath.row
+        let beer = m.cervezas[n]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RowCell")
+        as! RowCell
+        if let foto = beer.foto {
+            cell.beerPhoto?.image = foto;
+        }
+        cell.beerName?.text = beer.name;
+        return cell
+    }
+    
 
 }
 
